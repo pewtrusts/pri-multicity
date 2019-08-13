0962e07e0f0837e9dbc2bf87e9c7b29140d37673
John Osterman <josterman@pewtrusts.org>
2019-08-13 14:01:26 -0400

    Radio buttons working to select view type

--- src/components/selections.svelte
+++ src/components/selections.svelte
@@ -1,6 +1,17 @@
 <script>
+    import { onMount } from 'svelte';
+    import viewTypeStore from './../store.js';
     import Loading from './loading.svelte';
     export let dataPromise;
+    let typeSelectors
+    function changeHandler(e){
+        viewTypeStore.set(e.target.value)
+    }
+    onMount(() => {
+        typeSelectors.querySelectorAll('input').forEach(input => {
+            input.addEventListener('change', changeHandler);
+        });
+    });
 </script>
 
 <style lang="scss">
@@ -13,9 +24,19 @@
         width: 33%;
         height: 40px;
     }
+     .view-type-selectors {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        label {
+            margin-left: 0.3em;
+            position: relative;
+            top: .125em;
+        }
+    } 
 </style>
 
-<div class="selections wire">
+<div class="selections">
 {#await dataPromise}
     <div>
         <Loading />
@@ -29,7 +50,10 @@
 {:then value}
     <div></div>  <!-- different selection types go here -->      
     <div></div>        
-    <div></div>        
+    <div bind:this="{typeSelectors}" class="view-type-selectors">
+        <div><input type="radio" name="view-type" value="time" id="radio1" checked="true" /><label for="radio1">Over time</label></div>
+        <div><input type="radio" name="view-type" value="disaggregated" id="radio2" /><label for="radio2">By age and race</label></div>
+    </div>        
 {:catch error}
     <p>Oops. Something went wrong</p>
     <code>{error.message}</code>
