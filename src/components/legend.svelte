05b349b213997ef6d64058d1714fd3ae554deb1f
John Osterman <josterman@pewtrusts.org>
2019-08-16 11:03:12 -0400

    Legend is in

--- src/components/legend.svelte
+++ src/components/legend.svelte
@@ -5,14 +5,22 @@ export let metadata;
 export let maxRadius;
 export let minRadius;
 
+
+
+function convertToMagnitude(n) {
+    var order = Math.round(Math.log(n) / Math.LN10
+                       + 0.000000001); // because float math sucks like that
+    return Math.pow(10,order);
+}
+
 let zScaleDomain = [metadata.minPop, metadata.maxPop];
-let viewBoxHeight = 75;
+let viewBoxHeight = maxRadius * 2 + 12;
 
 let margin = {
-    top: 2,
-    right: 5,
-    bottom: 10,
-    left: 5
+    top: 0,
+    right: 0,
+    bottom: 7,
+    left: 2
 };
 let svg;
 
@@ -21,28 +29,119 @@ onMount(() => {
     const width = 100 - margin.left - margin.right;
     const height = viewBoxHeight - margin.top - margin.bottom;
     const zScale = d3.scaleSqrt().range([minRadius, maxRadius]).domain(zScaleDomain);
-    const data = zScaleDomain;
+    const data = zScaleDomain.map(d => convertToMagnitude(d)).concat([ minRadius / 2]);
 
-    d3.select(svg).selectAll('circle')
+    var group = d3.select(svg).selectAll('g')
         .data(data)
-        .enter().append('circle')
-        .attr('cx', d => zScale(d))
-        .attr('cy', d => height - zScale(d))
-        .attr('r', d => zScale(d));
+        .enter().append('g')
+        .attr('transform', (d,i) => i !== 2 ? `translate(${margin.left + +zScale(data[1])}, ${height - zScale(d)})` : `translate(${margin.left + +zScale(data[1])},${height - d})`);
+        
+    group.append('circle')
+        .attr('r', (d,i) => i === 2 ? d : zScale(d));
+
+    group.append('text')
+        .attr('class', 'circle-label')
+        .attr('transform', (d,i) => i !== 2 ? `translate(0, ${0 - +zScale(d) - 2})` : `translate(0, ${d * 2 + 4} )`)
+        .text((d,i) => i !== 2 ? d3.format('.0s')(d) : 'n/a');
+        
+
+
 
 }); // end onMount
 </script>
 
 <style lang="scss">
-    .legend {
-        width: 198px;
-        position: absolute;
-        :global(circle) {
-            fill: #000;
-            fill-opacity: 1;
+    @import './../variables.scss';
+    .legend{
+        position: fixed;
+        background-color: #fff;
+        padding:5px;
+        bottom: 0;
+        right: 0;
+        display: flex;
+        border: 1px solid $blue;
+        .color-codes {
+            display: flex;
+            flex-direction: column;
         }
     }
-</style>
+    .population-legend {
+        width: 62px;
+        :global(circle) {
+            fill: none;
+            stroke: $blue;
+        }
+        :global(.circle-label) {
+            font-size: 7.5px;
+            text-anchor: middle;
+        }
+    }
+    h3 {
+        font-size: 16px;
+        margin: 0;
+    }
+    ul {
+      margin:0;
+      padding: 0;
+      text-indent: 0;
+      list-style-type: none;
+    }
+    li {
+        display: inline-block;
+        margin-right: 8px;
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 12px;
+            width: 12px;
+            border-radius: 6px;
+            margin-right: 4px;
+        }
+        &:nth-child(1) {
+            &::before {
+                background-color: $teal;
+            }
+        }
+        &:nth-child(2) {
+            &::before {
+                background-color: $orange ;
+            }
+        }
+        &:nth-child(3) {
+            &::before {
+                background-color: $red;
+            }
+        }
+        &:nth-child(4) {
+            &::before {
+                background-color: $light_blue;
+            }
+        }
+        
+    }
 
-<svg class="legend" bind:this={svg} width="100%" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 100 {viewBoxHeight}"></svg>
+</style>
+<div class="legend">
+    <div class="color-codes">
+        <div>
+            <h3>Age:</h3>
+            <ul>
+                <li>&lt;35</li>
+                <li>35–64</li>
+                <li>64+</li>
+            </ul>
+        </div>
+        <div>
+            <h3>Race:</h3>
+            <ul>
+                <li>White, non-Hispanic</li>
+                <li>Black</li>
+                <li>Hispanic</li>
+                <li>Asian</li>
+            </ul>
+        </div>
+    </div>
+    <svg class="population-legend" bind:this={svg} width="100%" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 {100/3.19} {viewBoxHeight}"></svg>
+</div>
     
+
