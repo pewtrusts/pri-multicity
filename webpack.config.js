b27609de367e61ce2c696c1aa47788fc6f89c587
John Osterman <josterman@pewtrusts.org>
2019-08-28 09:14:52 -0400

    Updates repos name; font issue

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const outputFolder = process.env.NODE_ENV === 'preview' ? 'docs/' : process.env.
 const isDev = mode === 'development';
 const isProd = process.env.NODE_ENV === 'production';
 
-const repoName = 'multicity';
+const repoName = 'pri-multicity';
 const publicPath = isProd ? '/~/media/data-visualizations/interactives/2019/multicity/' : '';
 
 console.log(sass);
